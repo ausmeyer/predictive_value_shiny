3f1153cbbb2014c33b469918c60f49f6170d54ed
ausmeyer <austin.g.meyer@gmail.com>
2016-03-11 19:21:41 -0600

    Remove extra garbage.

--- ui.R
+++ ui.R
@@ -1,12 +1,6 @@
 library(shiny)
 
 ## Define question and answer choices
-Question <- "Investigators are studying prostate specific antigen (PSA) as a predictor for prostate cancer. To make the statistics easier, they are going to assume that PSA is a normally distributed population variable. Which of the following is correct under their assumption?"
-Answer1 <- "Mode is greater than median"
-Answer2 <- "Median is greater than mode"
-Answer3 <- "95% CI depends on degrees of freedom"
-Answer4 <- "Median is equal to mean"
-Answer5 <- "Mean is equal to standard deviation"
 
 # Define UI for miles per gallon application
 shinyUI(fluidPage(
