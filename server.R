4e6789293e37a0bc8c3ff99d3e948084acae6e46
ausmeyer <austin.g.meyer@gmail.com>
2016-03-12 21:24:17 -0600

    The functions are working at this point.

--- server.R
+++ server.R
@@ -1,63 +1,309 @@
 library(shiny)
 
 # Define server logic required to generate and plot a random distribution
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
   
-  output$ppvPlot <- renderPlot({
-    library(ggplot2)
-    library(grid)
-    library(gridExtra)
-    library(cowplot)
+  calc.metrics <- reactive({
+    prevalence.point <- input$prev
+    n <- input$n
+    with.disease <- prevalence.point * n
+    without.disease <- n - with.disease
+    
+    calc.sensitivity <- function() {
+      with.disease <- prevalence.point * n
+      with.disease.distribution <- rnorm(with.disease, mean = input$disease_mean, sd = input$disease_spread)
+      TP <- sum(with.disease.distribution >= input$cutoff)
+      TP / with.disease
+    }
+    
+    calc.specificity <- function() {
+      without.disease <- n - prevalence.point * n
+      without.disease.distribution <- rnorm(without.disease, mean = input$no_disease_mean, sd = input$no_disease_spread)
+      TN <- sum(without.disease.distribution < input$cutoff)
+      TN / without.disease
+    }
+    
+    calculated.sensitivity <- calc.sensitivity()
+    calculated.specificity <- calc.specificity()
+    
+    return(c(calculated.sensitivity, calculated.specificity))
+  })
+  
+  calc.NPV <- reactive({
+    prevalence <- seq(0, 1, by = 0.01)
+    prevalence.point <- input$prev
+    n <- input$n
     
     sensitivity <- input$sens
     specificity <- input$spec
     
-    prevalence <- seq(0, 1, by = 0.01)
-    n <- 1000
-    
-    TP <- n * prevalence
-    TN <- n - TP
-    FP <- n * (1 - specificity)
-    FN <- n * prevalence * (1 - sensitivity)
-    PPV <- TP / (TP + FP)
+    TP <- n * prevalence * sensitivity                           # people testing correctly positive
+    TN <- (n - (n * prevalence)) * specificity                   # people testing correctly negative
+    FP <- (n - (n * prevalence)) * (1 - specificity)             # people who do not have disease but test positive
+    FN <- n * prevalence * (1 - sensitivity)                     # people who have disease but test negative
     NPV <- TN / (TN + FN)
-    
-    data <- data.frame(x = prevalence, y = PPV)
-    ppvplot <- ggplot(data, aes(x = x, y = y)) + 
-      geom_line() +
-      ylim(0, 1) + 
-      ggtitle("Positive Predictive Value versus Prevalence") +
-      labs(x="Prevalence", y="Positive Predictive Value")
-    
-    show(ppvplot)
   })
   
-  output$npvPlot <- renderPlot({
-    library(ggplot2)
-    library(grid)
-    library(gridExtra)
-    library(cowplot)
+  calc.PPV <- reactive({
+    prevalence <- seq(0, 1, by = 0.01)
+    prevalence.point <- input$prev
+    n <- input$n
     
     sensitivity <- input$sens
     specificity <- input$spec
     
-    prevalence <- seq(0, 1, by = 0.01)
-    n <- 1000
-    
-    TP <- n * prevalence
-    TN <- n - TP
-    FP <- n * (1 - specificity)
-    FN <- n * prevalence * (1 - sensitivity)
+    TP <- n * prevalence * sensitivity                           # people testing correctly positive
+    TN <- (n - (n * prevalence)) * specificity                   # people testing correctly negative
+    FP <- (n - (n * prevalence)) * (1 - specificity)             # people who do not have disease but test positive
+    FN <- n * prevalence * (1 - sensitivity)                     # people who have disease but test negative
     PPV <- TP / (TP + FP)
-    NPV <- TN / (TN + FN)
-    
-    data <- data.frame(x = prevalence, y = NPV)
-    npvplot <- ggplot(data, aes(x = x, y = y)) + 
-      geom_line() +
-      ylim(0, 1) + 
-      ggtitle("Negative Predictive Value versus Prevalence") +
-      labs(x="Prevalence", y="Negative Predictive Value")
-    
-    show(npvplot)
   })
+  
+  renderPop.changedPop <- reactive({
+    library(ggplot2)
+    library(cowplot)
+    library(grid)
+    
+    prevalence.point <- input$prev
+    n <- input$n
+    with.disease <- prevalence.point * n
+    without.disease <- n - with.disease
+    
+    sensitivity <- round(calc.metrics()[1], digits = 2)
+    specificity <- round(calc.metrics()[2], digits = 2)
+    
+    plot.function <- function(disease.distribution, no.disease.distribution, sens, spec, test_cutoff) {
+      prevalence.plot <- ggplot(data.frame(data = c(disease.distribution, no.disease.distribution), 
+                                           Groups = c(rep('Disease', length(disease.distribution)), 
+                                                      rep('No Disease', length(no.disease.distribution)))), 
+                                aes(x=data, fill=Groups)) + 
+        geom_histogram(alpha = 0.2, position="identity", bins = 50) + 
+        geom_vline(xintercept = test_cutoff, linetype = 'solid', lwd = 0.5) +
+        theme(plot.margin = unit(c(2,1,1,1), "lines")) + 
+        scale_fill_manual(values = c("No Disease" = "blue", "Disease" = "red")) +
+        xlim(-5, 25) +
+        labs(x="Clinical Test Result", y="Number of Patients") +
+        annotate("text", label = paste("Calculated Sensitivity: ", sens, sep = ""), x = -Inf, y = Inf, hjust = 0, vjust = -1) +
+        annotate("text", label = paste("Calculated Specificity: ", spec, sep = ""), x = Inf, y = Inf, hjust = 1, vjust = -1)
+      
+      gg2 <- ggplot_gtable(ggplot_build(prevalence.plot))
+      gg2$layout$clip[gg2$layout$name == "panel"] <- "off"
+      grid.draw(gg2)
+    }
+    
+    with.disease.distribution <- rnorm(with.disease, mean = input$disease_mean, sd = input$disease_spread)
+    without.disease.distribution <- rnorm(without.disease, mean = input$no_disease_mean, sd = input$no_disease_spread)
+    
+    plot.function(with.disease.distribution, without.disease.distribution, sensitivity, specificity, input$cutoff)
+  })
+  
+  renderPop.constantPop <- reactive({
+    library(ggplot2)
+    library(cowplot)
+    library(grid)
+    
+    prevalence.point <- input$prev
+    n <- input$n
+    with.disease <- prevalence.point * n
+    without.disease <- n - with.disease
+    
+    sensitivity <- input$sens
+    specificity <- input$spec
+    
+    plot.function <- function(disease.distribution, no.disease.distribution, sens, spec, test_cutoff) {
+      prevalence.plot <- ggplot(data.frame(data = c(disease.distribution, no.disease.distribution), 
+                                           Groups = c(rep('Disease', length(disease.distribution)),
+                                                      rep('No Disease', length(no.disease.distribution)))), 
+                                aes(x=data, fill=Groups)) + 
+        geom_histogram(alpha = 0.2, position="identity", bins = 50) + 
+        geom_vline(xintercept = test_cutoff, linetype = 'solid', lwd = 0.5) +
+        theme(plot.margin = unit(c(2,1,1,1), "lines")) + 
+        scale_fill_manual(values = c("No Disease" = "blue", "Disease" = "red")) +
+        xlim(-5, 25) +
+        labs(x="Clinical Test Result", y="Number of Patients") +
+        annotate("text", label = paste("Calculated Sensitivity: ", sens, sep = ""), x = -Inf, y = Inf, hjust = 0, vjust = -1) +
+        annotate("text", label = paste("Calculated Specificity: ", spec, sep = ""), x = Inf, y = Inf, hjust = 1, vjust = -1)
+      
+      gg2 <- ggplot_gtable(ggplot_build(prevalence.plot))
+      gg2$layout$clip[gg2$layout$name == "panel"] <- "off"
+      grid.draw(gg2)
+    }
+    
+    fn <- function(x) {
+      tmp.with.disease <- rnorm(with.disease, mean = x[1], sd = x[2])
+      TP <- sum(tmp.with.disease >= x[5])         # people testing correctly positive
+      
+      tmp.without.disease <- rnorm(without.disease, mean = x[3], sd = x[4])
+      TN <- sum(tmp.without.disease < x[5])       # people testing correctly negative
+      
+      return((sensitivity - TP / with.disease)^2 + (specificity - TN / without.disease)^2)
+    }
+    
+    optim.distribution <- optim(par = c(10, 2, 10, 2, 10), fn)
+    
+    with.disease.distribution <- rnorm(with.disease, mean = optim.distribution$par[1], sd = optim.distribution$par[2])
+    without.disease.distribution <- rnorm(without.disease, mean = optim.distribution$par[3], sd = optim.distribution$par[4])
+    
+    print(mean(with.disease.distribution))
+    TP <- sum(with.disease.distribution >= optim.distribution$par[5])
+    calculated.sensitivity <- round(TP / with.disease, digits = 2)
+    print(mean(without.disease.distribution))
+    TN <- sum(without.disease.distribution < optim.distribution$par[5])
+    calculated.specificity <- round(TN / without.disease, digits = 2)
+    
+    updateSliderInput(session, "disease_mean", value = mean(with.disease.distribution))
+    updateSliderInput(session, "disease_spread", value = sd(with.disease.distribution))
+    updateSliderInput(session, "no_disease_mean", value = mean(without.disease.distribution))
+    updateSliderInput(session, "no_disease_spread", value = sd(without.disease.distribution))
+    updateSliderInput(session, "cutoff", value = optim.distribution$par[5])
+    
+    plot.function(with.disease.distribution, without.disease.distribution, calculated.sensitivity, calculated.specificity, optim.distribution$par[5])
+  })
+  
+  renderPPV.changedPop <- reactive({
+    library(ggplot2)
+    library(cowplot)
+    
+    plot.function <- function(prev, ppv, ppv.point) {
+      data <- data.frame(x = prevalence, y = PPV)
+      npvplot <- ggplot(data, aes(x = x, y = y)) + 
+        geom_line() +
+        geom_segment(x = prevalence.point, xend = prevalence.point, y = 0, yend = ppv.point, color = 'red', linetype = 'dotted') +
+        geom_segment(x = 0, xend = prevalence.point, y = ppv.point, yend = ppv.point, color = 'red', linetype = 'dotted') +
+        ylim(0, 1) + 
+        labs(x="Prevalence", y="Positive Predictive Value")
+      
+      show(npvplot)
+    }
+    
+    calc.PPV.local <- function(sens, spec) {
+      prevalence <- seq(0, 1, by = 0.01)
+      prevalence.point <- input$prev
+      n <- input$n
+      
+      sensitivity <- sens
+      specificity <- spec
+      
+      TP <- n * prevalence * sensitivity                           # people testing correctly positive
+      TN <- (n - (n * prevalence)) * specificity                   # people testing correctly negative
+      FP <- (n - (n * prevalence)) * (1 - specificity)             # people who do not have disease but test positive
+      FN <- n * prevalence * (1 - sensitivity)                     # people who have disease but test negative
+      PPV <- TP / (TP + FP)
+    }
+    
+    prevalence <- seq(0, 1, by = 0.01)
+    prevalence.point <- input$prev
+    
+    sensitivity <- round(calc.metrics()[1], digits = 2)
+    specificity <- round(calc.metrics()[2], digits = 2)
+    
+    PPV <- calc.PPV.local(sensitivity, specificity)
+    PPV.point <- PPV[prevalence.point * length(prevalence) + 1]
+    
+    updateSliderInput(session, "sens", value = sensitivity)
+    updateSliderInput(session, "spec", value = specificity)
+    
+    plot.function(prevalence, PPV, PPV.point)
+  })
+  
+  renderPPV.constantPop <- reactive({
+    library(ggplot2)
+    library(cowplot)
+    
+    plot.function <- function(prev, ppv, ppv.point) {
+      data <- data.frame(x = prevalence, y = PPV)
+      npvplot <- ggplot(data, aes(x = x, y = y)) + 
+        geom_line() +
+        geom_segment(x = prevalence.point, xend = prevalence.point, y = 0, yend = ppv.point, color = 'red', linetype = 'dotted') +
+        geom_segment(x = 0, xend = prevalence.point, y = ppv.point, yend = ppv.point, color = 'red', linetype = 'dotted') +
+        ylim(0, 1) + 
+        labs(x="Prevalence", y="Positive Predictive Value")
+      
+      show(npvplot)
+    }
+    
+    prevalence <- seq(0, 1, by = 0.01)
+    prevalence.point <- input$prev
+    
+    PPV <- calc.PPV()
+    PPV.point <- PPV[prevalence.point * length(prevalence) + 1]
+    
+    plot.function(prevalence, PPV, PPV.point)
+  })
+  
+  renderNPV.changedPop <- reactive({
+    library(ggplot2)
+    library(cowplot)
+    
+    plot.function <- function(prev, npv, npv.point) {
+      data <- data.frame(x = prevalence, y = NPV)
+      npvplot <- ggplot(data, aes(x = x, y = y)) + 
+        geom_line() +
+        geom_segment(x = prevalence.point, xend = prevalence.point, y = 0, yend = npv.point, color = 'red', linetype = 'dotted') +
+        geom_segment(x = 0, xend = prevalence.point, y = npv.point, yend = npv.point, color = 'red', linetype = 'dotted') +
+        ylim(0, 1) + 
+        labs(x="Prevalence", y="Negative Predictive Value")
+      
+      show(npvplot)
+    }
+    
+    calc.NPV.local <- function(sens, spec) {
+      prevalence <- seq(0, 1, by = 0.01)
+      prevalence.point <- input$prev
+      n <- input$n
+      
+      sensitivity <- sens
+      specificity <- spec
+      
+      TP <- n * prevalence * sensitivity                           # people testing correctly positive
+      TN <- (n - (n * prevalence)) * specificity                   # people testing correctly negative
+      FP <- (n - (n * prevalence)) * (1 - specificity)             # people who do not have disease but test positive
+      FN <- n * prevalence * (1 - sensitivity)                     # people who have disease but test negative
+      NPV <- TN / (TN + FN)
+    }
+    
+    prevalence <- seq(0, 1, by = 0.01)
+    prevalence.point <- input$prev
+    
+    sensitivity <- round(calc.metrics()[1], digits = 2)
+    specificity <- round(calc.metrics()[2], digits = 2)
+    
+    NPV <- calc.NPV.local(sensitivity, specificity)
+    NPV.point <- NPV[prevalence.point * length(prevalence) + 1]
+    
+    updateSliderInput(session, "sens", value = sensitivity)
+    updateSliderInput(session, "spec", value = specificity)
+    
+    plot.function(prevalence, NPV, NPV.point)
+  })
+  
+  renderNPV.constantPop <- reactive({
+    library(ggplot2)
+    library(cowplot)
+    
+    prevalence <- seq(0, 1, by = 0.01)
+    prevalence.point <- input$prev
+    NPV <- calc.NPV()
+    
+    plot.function <- function(prev, npv, npv.point) {
+      data <- data.frame(x = prevalence, y = NPV)
+      npvplot <- ggplot(data, aes(x = x, y = y)) + 
+        geom_line() +
+        geom_segment(x = prevalence.point, xend = prevalence.point, y = 0, yend = npv.point, color = 'red', linetype = 'dotted') +
+        geom_segment(x = 0, xend = prevalence.point, y = npv.point, yend = npv.point, color = 'red', linetype = 'dotted') +
+        ylim(0, 1) + 
+        labs(x="Prevalence", y="Negative Predictive Value")
+      
+      show(npvplot)
+    }
+    
+    NPV <- calc.NPV()
+    NPV.point <- NPV[prevalence.point * length(prevalence) + 1]
+    
+    plot.function(prevalence, NPV, NPV.point)
+  })
+  
+  output$prevalencePlot <- renderPlot(renderPop.changedPop())
+  output$ppvPlot <- renderPlot(renderPPV.changedPop())
+  output$npvPlot <- renderPlot(renderNPV.changedPop())
 })
--- ui.R
+++ ui.R
@@ -1,25 +1,65 @@
 library(shiny)
 
-## Define question and answer choices
-
-# Define UI for miles per gallon application
-shinyUI(fluidPage(
-  
-  # Application title
-  headerPanel("Predictive Value of Tests"),
-  
-  sidebarLayout(
+shinyUI(
+  fluidPage(
     
-    sidebarPanel(
-      sliderInput("sens", "Sensitivity",  
-                  min = 0, max = 1, value = 0.5),
-      sliderInput("spec", "Specificity",  
-                  min = 0, max = 1, value = 0.5)
+    # Application title
+    title = "Predictive Value of Clinical Tests",
+    
+    fluidRow(column(6, plotOutput('prevalencePlot')), column(3, plotOutput('ppvPlot')), column(3, plotOutput('npvPlot'))),
+    
+    hr(),
+    
+    fluidRow(
+      column(3,
+             sliderInput("n", "Population Size",  
+                         min = 0, max = 20000, value = 2000, step = 1)
+      ),
+      column(3,
+             sliderInput("prev", "Prevalence",  
+                         min = 0, max = 1, value = 0.1, step = 0.01)
+      ),
+      column(3,
+             sliderInput("sens", "Sensitivity",  
+                         min = 0, max = 1, value = 0.5, step = 0.01)
+      ),
+      column(3,
+             sliderInput("spec", "Specificity",  
+                         min = 0, max = 1, value = 0.5, step = 0.01)
+      )
     ),
     
-    mainPanel(
-      plotOutput('ppvPlot'),
-      plotOutput('npvPlot')
-    )
+    hr(),
+    
+    fluidRow(
+      column(3,
+             sliderInput("no_disease_mean", "No Disease Mean",  
+                         min = 0, max = 20, value = 10, step = 0.01)
+      ),
+      column(3,
+             sliderInput("no_disease_spread", "No Disease Spread",  
+                         min = 0.1, max = 4, value = 2, step = 0.01)
+      ),
+      column(3,
+             sliderInput("disease_mean", "Disease Mean",  
+                         min = 0, max = 20, value = 10, step = 0.01)
+      ),
+      column(3,
+             sliderInput("disease_spread", "Disease Spread",  
+                         min = 0.1, max = 4, value = 2, step = 0.01)
+      )
+    ),
+    
+    hr(),
+    
+    fluidRow(
+      column(3,
+             sliderInput("cutoff", "Cutoff",  
+                         min = 0, max = 20, value = 10, step = 0.01)
+      )
+    ),
+    
+    p("This simple widget is meant to show the affect that changing prevalence, sensitivity and specificity has on positive predictive value, negative predictive value and the distribution of data. Currently, the means, spreads and cutoffs are only for display. They do not affect the visualization or other parameters. In addition, the test distributions are fit based on the sensitivity and specificity. As a result, I have included the calculated sensitivity and specificity for the actual distributions displayed on the left.")
+    
   )
-))
+)
